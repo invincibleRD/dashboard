3e04b5296e3aca38243fa452b8be06a7c5f57b6b
DIPENDRA KUMAR SHAH <74634983+invincibleRD@users.noreply.github.com>
2023-04-19 22:58:55 +0530

    fetched data and implemetned it in graph

--- pages/components/dashboard/header.js
+++ pages/components/dashboard/header.js
@@ -8,7 +8,7 @@ export default function Header(){
                 <div style={{display:"flex"}}>
               <input placeholder="Search..."/>
               <FontAwesomeIcon style={{height:"30px"}} icon={faBell}></FontAwesomeIcon>
-              <img  className ="dot1" src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwJzyQcBu_7EH0wSSW14L4Edxsj3X8AJKcy3UBpQE33iMrZ8Z3SNXmPpEZyqsl898vYwI&usqp=CAU'} alt = {"this is an image"} />
+              <img  className ="dot1" src={'https://www.w3schools.com/howto/img_avatar.png'} alt = {"this is an image"} />
              </div>
              </div>
         </>
